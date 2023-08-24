0783a9fa3c86955980db937323f8627c834e406f
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-08-24 19:44:09 -0400

    done base function/layout for sidebars

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -4,9 +4,17 @@ import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+import { User } from "@prisma/client";
+
+interface DesktopSidebarProps {
+    currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
     const routes = useRoutes(); // make sure using the one from created custom hook
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log({ currentUser });
 
     return (
         <div
